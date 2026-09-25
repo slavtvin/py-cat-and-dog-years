47ef8c7e1fcda23d6ee450c9dc5b521813a78263
Slavtvin <koffman06@gmail.com>
2026-09-25 19:57:17 +0300

    Solution 1

--- app/test_main.py
+++ app/test_main.py
@@ -3,13 +3,14 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (0, 0, [0, 0]),
     (14, 14, [0, 0]),
     (15, 15, [1, 1]),
-    (18, 18, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
     (28, 28, [3, 2]),
-    (29, 29, [3, 3]),
-    (100, 100, [21, 17]),
-    (28, 29, [3, 3])
+    (100, 100, [21, 17])
 ])
 def test_animal_if_age_less_15(cat_age: int,
                                dog_age: int,
